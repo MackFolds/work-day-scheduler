836636c8ed4dd53f4ec3d4c30f5f3778c7c48c71
Mack Folds <treyfolds93@gmail.com>
2021-04-16 21:02:31 -0600

    add remaining javascript

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,23 @@
-
-    
-$("#currentDay").text(moment().format("[Today Is] dddd, MMMM Do YYYY, h:mm a"));
-
-$(".saveBtn").on("click", function (){
-    console.log(this);
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-    localStorage.setItem(time, text);
-});
-
+// start to pull previous entries
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
 $("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-$("#hour2 .description").val(localStorage.getItem("hour2"));
-$("#hour3 .description").val(localStorage.getItem("hour3"));
-$("#hour4 .description").val(localStorage.getItem("hour4"));
-$("#hour5 .description").val(localStorage.getItem("hour5"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
+// give current day
+$("#currentDay").text(moment().format("[Today Is] dddd, MMMM Do YYYY, h:mm a"));
+
+// check each block to see if we have passed that hour
 function hourTracker() {
     var currentHour = moment().hour();
 
     $(".time-block").each(function () {
         var block = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(this)
 
         if (block < currentHour) {
             $(this).addClass("past");
@@ -44,3 +37,11 @@ function hourTracker() {
     });
 }
 hourTracker();
+
+// save entry
+$(".saveBtn").on("click", function (){
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+});
